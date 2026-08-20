54a3c9afec2e5c91d76816fd2e665f6c83854409
tariqbaater <tariqbaater@gmail.com>
2026-08-20 19:20:07 +0300

    removed neocodium changes that were blocking updates

--- nvim/.config/nvim/lua/tariq/plugins/neocodium.lua
+++ nvim/.config/nvim/lua/tariq/plugins/neocodium.lua
@@ -12,6 +12,22 @@ return {
         lua = true,
       },
     })
+
+    -- Runtime overrides (no plugin file edits, so updates work):
+    local filetype = require("neocodeium.filetype")
+    filetype.aliases.mysql = "sql"
+
+    local stdio = require("neocodeium.utils.stdio")
+    local doc = require("neocodeium.doc")
+    local orig_get = doc.get
+    doc.get = function(buf, ft, max_lines, pos)
+      local data = orig_get(buf, ft, max_lines, pos)
+      local name = vim.api.nvim_buf_get_name(buf)
+      local abs_name = vim.fn.fnamemodify(name, ":p")
+      data.absolute_uri = stdio.to_uri(abs_name)
+      data.workspace_uri = stdio.to_uri(vim.fn.fnamemodify(abs_name, ":h"))
+      return data
+    end
     -- vim.keymap.set("i", "<a-f>", neocodeium.accept)
     vim.keymap.set("i", "<Tab>", function()
       require("neocodeium").accept()
