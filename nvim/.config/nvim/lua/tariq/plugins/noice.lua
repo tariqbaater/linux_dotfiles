0dd423f53c99a7cf7ea69f89ef6719fdf9c5efb4
tariqbaater <tariqbaater@gmail.com>
2026-08-24 10:40:59 +0300

    silenced annoying error whenever i apply themes

--- nvim/.config/nvim/lua/tariq/plugins/noice.lua
+++ nvim/.config/nvim/lua/tariq/plugins/noice.lua
@@ -1,33 +1,40 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    config = function()
-        require("noice").setup()
-    end,
-    opts = {
-        -- add any options here
-        cmdline = {
-            view = "cmdline_popup",
-        },
-        messages = {
-            enabled = true,
-            view = "mini",
-        },
-        lsp = {
-            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-            },
-        },
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("noice").setup()
+  end,
+  opts = {
+    -- add any options here
+    cmdline = {
+      view = "cmdline_popup",
     },
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
+    messages = {
+      enabled = true,
+      view = "mini",
     },
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    {
+      "rcarriga/nvim-notify",
+      config = function()
+        require("notify").setup({
+          background_colour = "#000000",
+        })
+      end,
+    },
+  },
 }
