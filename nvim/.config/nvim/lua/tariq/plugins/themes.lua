9849570eae6cf0d8400cd1f39e145847b2f9cc5f
tariqbaater <tariqbaater@gmail.com>
2026-08-24 10:42:31 +0300

    themes now dynamically change according to the system's theme

--- nvim/.config/nvim/lua/tariq/plugins/themes.lua
+++ nvim/.config/nvim/lua/tariq/plugins/themes.lua
@@ -2,27 +2,27 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin-mocha",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,
     config = function()
       require("catppuccin").setup({
         flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = {     -- :h background
+        background = { -- :h background
           light = "latte",
           dark = "mocha",
         },
-        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
-        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = false,             -- dims the background color of inactive window
+          enabled = false, -- dims the background color of inactive window
           shade = "dark",
-          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false,             -- Force no italic
-        no_bold = false,               -- Force no bold
-        no_underline = false,          -- Force no underline
-        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" },     -- Change the style of comments
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
           conditionals = { "italic" },
           loops = {},
           functions = {},
@@ -58,7 +58,35 @@ return {
         },
       })
 
-      vim.cmd.colorscheme "catppuccin-mocha" -- prioritize catppuccin-macchiato over other colorschemes
+
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          spells = true,
+          strings = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statusline, errors
+        palette_overrides = {},
+        dim_unfocused = false, -- dims the background color of inactive windows
+      })
     end,
   },
 }
